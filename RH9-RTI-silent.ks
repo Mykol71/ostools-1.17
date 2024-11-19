7d655e09dfcd7e9c7e1c6b1907881254e528009a
Mike Green <mgreen@mykol.com>
2024-11-18 22:01:47 -0600

    rh9 rh10 start

---
+++ RH9-RTI-silent.ks
@@ -0,0 +1,119 @@
+#version=RHEL9
+text
+authselect --passalgo=sha512 --useshadow
+selinux --permissive
+reboot
+bootloader --append="video=640x480 net.ifnames=0"
+repo --name="AppStream" --baseurl=http://rhel8repo.centralus.cloudapp.azure.com/rhel-8-for-x86_64-appstream-rpms
+repo --name="BaseOS" --baseurl=http://rhel8repo.centralus.cloudapp.azure.com/rhel-8-for-x86_64-baseos-rpms
+
+%packages
+@^minimal-environment
+@system-tools
+aide
+audispd-plugins
+libreswan
+opensc
+openscap
+openscap-scanner
+pcsc-lite
+scap-security-guide
+net-tools
+java
+perl
+wget
+mailx
+procmail
+bind-utils
+ksh
+expect
+-firewalld
+iptables
+iptables-services
+samba
+cups
+chrony
+ntpstat
+telnet
+fetchmail
+rsyslog
+python2
+rear
+java
+sed
+lrzsz
+ncurses-term
+rsync
+grub2-efi-x64-modules
+grub2-tools-efi
+sendmail
+crypto-policies-scripts
+mutt
+NetworkManager
+%end
+
+# Keyboard layouts
+keyboard --xlayouts='us'
+# System language
+lang en_US.UTF-8
+
+# Network information
+network  --bootproto=dhcp --device=eth0 --ipv6=no --activate
+network  --hostname=rhel8-rti.teleflora.com
+
+url --url=http://rhel8repo.centralus.cloudapp.azure.com/rhel-8-for-x86_64-baseos-rpms
+
+# Run the Setup Agent on first boot
+firstboot --disable
+
+ignoredisk --only-use=sda
+# Partition clearing information
+zerombr
+clearpart --all --initlabel
+# Disk partitioning information
+part /usr2 --fstype="xfs" --ondisk=sda --grow
+part swap --fstype="swap" --ondisk=sda --recommended
+part /boot/efi --fstype="efi" --ondisk=sda --size=600 --fsoptions="umask=0077,shortname=tflinux"
+part / --fstype="xfs" --ondisk=sda --size=31024
+
+# System timezone
+timezone America/Winnipeg --isUtc
+
+# Root password
+#rootpw --iscrypted $2b$10$jBk4hLcfILSSTDA5m7EjduMFKYKjBLfCppM4QUsWZF/JbXVmxpqbi
+rootpw --iscrypted $6$G/P1TGIsGMWZ9aak$Sm/HZ1fcdfdmTdy4k2BTMv9Sw.mQOhrgtvMD5e8oo7t3uCtX2T005e/afw46a6TkODKkP8b9SrUgSAnjKxxfi1
+user --groups=wheel --name=tfsupport --password=$6$G/P1TGIsGMWZ9aak$Sm/HZ1fcdfdmTdy4k2BTMv9Sw.mQOhrgtvMD5e8oo7t3uCtX2T005e/afw46a6TkODKkP8b9SrUgSAnjKxxfi1 --iscrypted --gecos="tfsupport"
+
+%addon com_redhat_kdump --disable --reserve-mb='auto'
+%end
+
+#%addon org_fedora_oscap
+#    content-type = scap-security-guide
+#    datastream-id = scap_org.open-scap_datastream_from_xccdf_ssg-rhel8-xccdf-1.2.xml
+#    xccdf-id = scap_org.open-scap_cref_ssg-rhel8-xccdf-1.2.xml
+#    profile = xccdf_org.ssgproject.content_profile_pci-dss
+#%end
+
+
+%anaconda
+pwpolicy root --minlen=6 --minquality=1 --notstrict --nochanges --notempty
+pwpolicy user --minlen=6 --minquality=1 --notstrict --nochanges --emptyok
+pwpolicy luks --minlen=6 --minquality=1 --notstrict --nochanges --notempty
+%end
+
+%post
+update-crypto-policies --set LEGACY
+sed -i "s/enabled=1/enabled=0/g" /etc/yum/pluginconf.d/subscription-manager.conf
+cd /usr/bin
+curl -O http://rhel8repo.centralus.cloudapp.azure.com/ostools-1.17/updateos updateos
+chmod +x /usr/bin/updateos
+systemctl start sendmail
+systemctl start smb
+systemctl start cups
+systemctl start iptables
+systemctl enable sendmail
+systemctl enable smb
+systemctl enable cups
+systemctl enable iptables
+openvt -s -w -- /usr/bin/updateos stage
+%end
---
+++ rh10/update_blm.pl
@@ -0,0 +1,285 @@
+#!/usr/bin/perl
+#
+# $Revision: 1.1 $
+# Copyright 2023 Teleflora.
+#
+# update_blm.pl
+#
+#DESC Update Basis BLM for correct license address
+#
+
+use strict;
+use warnings;
+use English;
+use Getopt::Long;
+use POSIX;
+
+
+my $tmpDir = "/tmp/";
+my $licType = "install";
+
+my $BLM_Address = "";
+my $BLM_Remote = "false";
+my $BLM_StartType = "init";
+
+my $url = "http://tposlinux.blob.core.windows.net/rtibbjupdate";
+my $blm19File = "BLM19_06-16-2022_0858.jar";
+my $blmFile = "BLM21_06-15-2022_0933.jar";
+my $urlChk = "/usr2/basis/blm/basis";
+my $HostIP = "";
+my $returnval = "";
+my $POSDIR = "/usr2/bbx";
+my $chkLic = "";
+my $blmVer = "";
+my $blmChk = "";
+
+loginfo("");
+loginfo("Starting update_blm.pl " . '$Revision: 1.0 $' . "\n");
+
+# What IP to check blm
+open(PIPE, "hostname -i |");
+while(<PIPE>) {
+    $HostIP = $_;
+}
+close(PIPE);
+
+
+# conditions to skip install 
+# if $chkLic contains poweredbybbj and $blmVer contains blm21
+# if using remote blm
+# ?? if no license file  - skip installing 
+
+$chkLic=`strings $urlChk 2>/dev/null | grep 'license\.[bp]'`;
+chomp $chkLic;
+$blmVer=`strings $urlChk 2>/dev/null | grep 'blm[0-9][0-9]'`;
+chomp $blmVer;
+
+$blmChk=`ps -ef|grep usr2.basis.blm|grep -v grep`;
+chomp $blmChk;
+
+if ( $chkLic =~ /poweredbybbj/i && $blmVer =~ /blm21/i )
+{
+    loginfo("Already has poweredbybbj address and version 21 blm, exiting \n");
+    exit 0;
+}
+
+if (-f "/usr2/basis/basis.lic")
+{
+        open BBJLIC, "</usr2/basis/basis.lic";
+        while (<BBJLIC>) {
+                if ($BLM_Address eq "")
+                {
+                        if ($_ =~ /^server[ ]{1,}.{1,}/i )
+                        {
+                                my @blm_lic = split(' ', $_);
+                                $BLM_Address = $blm_lic[1];
+                                if ($BLM_Address =~ /localhost/i || $BLM_Address =~ /127.0.0.1/i || $BLM_Address =~ /$HostIP/i )
+                                {
+                                        $BLM_Address = "";
+                                }
+                        }
+                }
+        }
+        close BBJLIC;
+        if ("$BLM_Address" ne "")
+        {
+                $BLM_Remote = "true";
+                $licType = "blm";
+                #loginfo("Using remote blm server at $BLM_Address, exiting \n");
+                loginfo("Using remote blm server at $BLM_Address \n");
+                #exit 0;
+        }
+}
+
+my $licFile=`ls -1 /usr2/basis/blm/[0-9]*[OB][SB][HJ]*.lic 2>/dev/null|tail -1`;
+
+if ("$licFile" eq "" && "$blmChk" ne "")
+{
+    loginfo("Current license file not found, exiting");
+    exit 0;
+}
+  
+unless (-f "$tmpDir/$blmFile" ) {
+    mysystem("wget --proxy=off --cache=off --progress=dot:mega -O $tmpDir/$blmFile $url/$blmFile");
+}
+
+my $blmvars="#blm only install
+INSTALL_WIZARD=silent
+UAC_WIZARD=off
+LICENSE_SELECTION_WIZARD=silent
+LICENSE_REGISTER_WIZARD=off
+LICENSE_INSTALL_WIZARD=off
+BLM_CFG_STARTUP_WIZARD=off
+BLM_START_STOP_WIZARD=off
+FINISH_WIZARD=silent
+LANGUAGE=en
+SPLASH_IMAGE=
+INSTALL_LICENSE_AGREE=true
+INSTALL_TARGET_DIRECTORY_WIN=
+INSTALL_TARGET_DIRECTORY_NON_WIN=/usr2/basis/blm
+INSTALL_JAVA_DIRECTORY_WIN=
+INSTALL_JAVA_DIRECTORY_NON_WIN=/usr/java/latest
+INSTALL_CUSTOM_FEATURES=BASIS License Manager
+UAC_ELEVATE=
+# The following value can be [register] [install]. Default is [register]
+LICENSE_SELECTION_OPTION=$licType
+LICENSE_INSTALL_ENTERLICINFO=false
+LICENSE_INSTALL_LICENSEFILE=$licFile
+LICENSE_INSTALL_FEATURE=
+LICENSE_INSTALL_ENCRYPTCODE=
+LICENSE_INSTALL_LICREV=
+LICENSE_INSTALL_HOSTID=
+LICENSE_INSTALL_EXPDATE=
+LICENSE_INSTALL_CHECKSUM=
+LICENSE_INSTALL_NUMUSERS=
+LICENSE_INSTALL_SERIALNUM=
+#
+LICENSE_REGISTER_DEMOLIC=
+LICENSE_REGISTER_COMPANYNAME=
+LICENSE_REGISTER_FIRSTNAME=
+LICENSE_REGISTER_LASTNAME=
+LICENSE_REGISTER_EMAIL=
+LICENSE_REGISTER_FAX=
+LICENSE_REGISTER_PHONE=
+LICENSE_REGISTER_HOSTNAME=
+LICENSE_REGISTER_HOSTID=
+LICENSE_REGISTER_SERIALNUM=
+LICENSE_REGISTER_AUTHNUM=
+LICENSE_REGISTER_DEMOUSERCOUNT=
+LICENSE_REGISTER_DEMOSERIALNUM=
+LICENSE_REGISTER_DEMOAUTHNUM=
+LICENSE_REGISTER_REGMETHOD=
+LICENSE_REGISTER_DELMETHOD=
+LICENSE_REGISTER_COUNTRYUSACANADA=
+LICENSE_REGISTER_WANTINFO=
+LICENSE_REGISTER_NOTEBOOK=
+LICENSE_REGISTER_PHONEFILE=
+LICENSE_REGISTER_OTHERFILE=
+#
+BLM_CFG_STARTUP_TYPE_WIN=
+BLM_CFG_STARTUP_SERVICESTARTUPTYPE_WIN=
+BLM_CFG_STARTUP_TYPE_NON_WIN=
+BLM_START_STOP_STARTUP=";
+
+open(BBJINST, "> $tmpDir/blmvars.txt");
+print BBJINST $blmvars;
+close(BBJINST);
+
+#
+if (-f "$tmpDir/blmvars.txt" && -f "$tmpDir/$blmFile" )
+    {
+      if ("$blmChk" ne "")
+       {
+        # only stop/start blm if it was running to start
+        loginfo("Stopping Basis License Manager.....");
+        mysystem ("/sbin/service blm stop");
+       }
+
+        loginfo("Installing new License Manager...");
+        mysystem("/usr/java/latest/bin/java -jar $tmpDir/$blmFile -p $tmpDir/blmvars.txt");
+
+        if($returnval != 0) {
+            logerror("Terminating update_blm with return code $returnval.");
+            exit($returnval);
+        }
+}
+
+loginfo("Checking blank blm file for service command ...");
+if (-f "/etc/rc.d/init.d/blm" && ! -s "/etc/rc.d/init.d/blm" )
+    {
+        loginfo("Removing blank blm file from /etc/rc.d/init.d ");
+        mysystem("rm -f /etc/rc.d/init.d/blm");
+        if (-f "/etc/rc.d/init.d/bbj" && ! -s "/etc/rc.d/init.d/bbj" )
+            {
+                loginfo("Removing blank bbj file from /etc/rc.d/init.d ");
+                mysystem("rm -f /etc/rc.d/init.d/bbj");
+        }
+}
+
+if ("$blmChk" ne "")
+{
+  loginfo("Starting Basis License Manager.....");
+  mysystem ("/sbin/service blm start");
+}
+
+loginfo("Removing files...");
+mysystem("rm -f $tmpDir/$blmFile 2>/dev/null");
+mysystem("rm -f $tmpDir/blmvars.txt 2>/dev/null");
+mysystem("rm -f $tmpDir/update_blm.pl 2>/dev/null");
+
+$chkLic=`strings $urlChk 2>/dev/null | grep 'license\.[bp]'`;
+chomp $chkLic;
+$blmVer=`strings $urlChk 2>/dev/null | grep 'blm[0-9][0-9]'`;
+chomp $blmVer;
+
+if ( $chkLic =~ /poweredbybbj/i )
+{
+    loginfo("Success, has poweredbybbj address, blm: $blmVer \n");
+}
+
+
+loginfo("update_blm.pl complete ");
+
+exit(0);
+
+
+
+#
+#
+#### Loginfo  & logerror routines
+#
+#
+sub loginfo
+{
+	my $message = $_[0];
+	my $logtime = strftime("%Y-%m-%d %H:%M:%S", localtime());
+
+	chomp($message);
+	open LOGFILE, ">> $POSDIR/log/RTI-Patches.log";
+		print LOGFILE "$logtime ($0-$$) <I> $message\r\n";
+	close LOGFILE;
+
+	return "";
+}
+
+
+sub logerror
+{
+	my $message = $_[0];
+	my $logtime = strftime("%Y-%m-%d %H:%M:%S", localtime());
+
+	chomp($message);
+	open LOGFILE, ">> $POSDIR/log/RTI-Patches.log";
+		print LOGFILE "$logtime ($0-$$) <E> $message\r\n";
+	close LOGFILE;
+
+	return "";
+}
+
+#
+# Subroutine to run system commands.  This logs an entry.
+#
+sub mysystem
+{
+        my $command = $_[0];
+	my $LOG_FILE = "$POSDIR/log/RTI-Patches.log";
+
+        system($command . ">> $LOG_FILE 2>> $LOG_FILE");
+        $returnval = WEXITSTATUS($?);
+        loginfo($command . " Returned $returnval");
+
+        return ($returnval)
+}
+
+# logvars for debug
+sub logvars
+{
+    loginfo("vars set ");
+    loginfo("HostIP: " . $HostIP);
+    loginfo("chkLic: " . $chkLic);
+    loginfo("licType: " . $licType);
+    loginfo("licFile: " . $licFile);
+    my $junk = `ls -o $tmpDir/blmvars.txt $tmpDir/$blmFile 2>/dev/null`;
+    loginfo($junk);
+
+}
